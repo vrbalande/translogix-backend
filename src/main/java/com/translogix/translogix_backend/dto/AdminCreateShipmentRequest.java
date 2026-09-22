76e26ef62f472e140f21b086c871fa7a60284ce8
vrbalande <vbalande20@gmail.com>
2026-09-22 20:24:47 +0530

    Assign admin-created shipments to users

---
+++ src/main/java/com/translogix/translogix_backend/dto/AdminCreateShipmentRequest.java
@@ -0,0 +1,89 @@
+package com.translogix.translogix_backend.dto;
+
+public class AdminCreateShipmentRequest {
+
+    private String trackingNumber;
+    private String senderName;
+    private String receiverName;
+    private String origin;
+    private String destination;
+    private String shipmentType;
+    private String status;
+    private Double weight;
+    private String username;
+
+    public AdminCreateShipmentRequest() {
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getReceiverName() {
+        return receiverName;
+    }
+
+    public void setReceiverName(String receiverName) {
+        this.receiverName = receiverName;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getShipmentType() {
+        return shipmentType;
+    }
+
+    public void setShipmentType(String shipmentType) {
+        this.shipmentType = shipmentType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
