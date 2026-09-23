f31b3e8efb55e7641c8e9e3b87e85b912c7d00db
vrbalande <vbalande20@gmail.com>
2026-09-23 13:41:57 +0530

    Secure user response and fix shipment assignment

--- src/main/java/com/translogix/translogix_backend/entity/User.java
+++ src/main/java/com/translogix/translogix_backend/entity/User.java
@@ -1,5 +1,6 @@
 package com.translogix.translogix_backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 @Entity
@@ -13,50 +14,66 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
     private String role;
 
+
     public User() {
     }
 
-    public User(String username, String password, String role) {
+
+    public User(
+            String username,
+            String password,
+            String role
+    ) {
         this.username = username;
         this.password = password;
         this.role = role;
     }
+
 
     public Long getId() {
         return id;
     }
 
+
     public void setId(Long id) {
         this.id = id;
     }
+
 
     public String getUsername() {
         return username;
     }
 
+
     public void setUsername(String username) {
         this.username = username;
     }
+
 
     public String getPassword() {
         return password;
     }
 
+
     public void setPassword(String password) {
         this.password = password;
     }
+
 
     public String getRole() {
         return role;
     }
 
+
     public void setRole(String role) {
         this.role = role;
     }
+
 }
